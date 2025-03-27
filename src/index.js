429ec393a7c153ad9165711d9e2959325b29e2b7
DanielAlbuquerq <daniel_developer@hotmail.com>
2025-03-27 16:39:08 -0300

    dynamic components

--- src/index.js
+++ src/index.js
@@ -2,46 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const author = 'Jordan Moore';
+const title = 'Interesting Facts For Curious Minds';
+const imgSrc = 'https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg';
+
 function BookList() {
   return (
     <section className='booklist'>
       <Book />
       <Book />
       <Book />
-      <Book />
     </section>
   );
 }
-
 const Book = () => {
-  return (
-    <article className='book'>
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
-    alt='Interesting Facts For Curious Minds'
-  />
-);
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>;
-};
-const Author = () => {
   const inlineHeadingStyles = {
     color: '#617d98',
     fontSize: '0.75rem',
     marginTop: '0.5rem',
-  }
-  return <h4 style={inlineHeadingStyles}>Jordan Moore</h4>;
-}
-
+  };
+  return (
+    <article className='book'>
+      <img src={imgSrc} alt={title} />
+      <h2>{title}</h2>
+      <h4 style={inlineHeadingStyles}> {author} </h4>
+    </article>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<BookList />);
